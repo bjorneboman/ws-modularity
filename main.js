50f9f760b16ae89ee02e72650868f79739b23807
Lindsteez <lindgren.axel@hotmail.com>
2025-11-05 10:44:38 +0100

    convert of temp added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import { convert as convertLength } from "./convertLength.js";
+import { convert as convertTemp } from "./convertTemp.js"
 
 console.log(`10 meter = ${convertLength(10)} cm`)
+console.log(`37 °C = ${convertTemp(37)}°F`)
