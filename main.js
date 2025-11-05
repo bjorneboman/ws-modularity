c6acb4962795b1467b73f80e444407ddfc45a4a8
Drilon Shehu <drilonshehu99@hotmail.com>
2025-11-05 10:54:01 +0100

    weight.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { convert as convertLength } from "./convertLength.js";
 import { convert as convertTemp } from "./convertTemp.js"
+import { convert as convertWeight } from "./convertWeight.js";
 
 console.log(`10 meter = ${convertLength(10)} cm`)
 console.log(`37 °C = ${convertTemp(37)}°F`)
+console.log(`62 kg = ${convertWeight(62)} lbs`)
